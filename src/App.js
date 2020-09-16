9706edeb862ab251d8e46491c29c795c1dd59ce0
Patrick Brown <patrickbrown1312@gmail.com>
2020-09-15 17:40:42 -0700

    added basic three.js rendering

--- src/App.js
+++ src/App.js
@@ -1,14 +1,55 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
+import "./App.css";
+import * as THREE from "three";
 
-function App() {
+
+const styleCanvas = {
+  position: "absolute",
+  top: 0,
+  left: 0,
+};
+
+export default function App() {
+  const CamRef = useRef();
+  const RenderRef = useRef();
+  useEffect(() => {
+
+    var scene = new THREE.Scene();
+    CamRef.current = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+
+    const canvas = document.querySelector("#c");
+    RenderRef.current = new THREE.WebGLRenderer({ canvas });
+    RenderRef.current.setSize(
+      window.innerWidth,
+      window.innerHeight
+    );
+
+    const geometry = new THREE.BoxGeometry(2, 2, 2);
+    const material = new THREE.MeshBasicMaterial({ color: 0x0000ff });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    CamRef.current.position.z = 5;
+
+    const animate = () => {
+      requestAnimationFrame(animate);
+      cube.rotation.x = 0.5;
+      cube.rotation.y = 0.5;
+      RenderRef.current.render(scene, CamRef.current);
+    };
+
+    animate();
+  }, []);
+  
   return (
-    <div className="App">
-      <div className="App-header">
-        <h1>Title</h1>
-      </div>
+    <div>
+      <canvas style={styleCanvas} id="c" />
     </div>
   );
 }
-
-export default App;
